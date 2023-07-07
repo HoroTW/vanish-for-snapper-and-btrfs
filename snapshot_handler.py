6ab57c9b9d45869e9fd833b4e43023364c10c23e
HoroTW <horo@horotw.com>
2023-07-08 01:25:40 +0200

    Simplify snapshot_handler

--- snapshot_handler.py
+++ snapshot_handler.py
@@ -5,6 +5,8 @@ import subprocess
 from logger import logger
 import snapper_timer_control as stc
 
+permission_modified_snapshots = []  # stores the snapshots that have been made writable
+
 
 def find_read_only_snapshots(snapshot_dirs: list[pathlib.Path]) -> list[pathlib.Path]:
     """Finds the read-only snapshots in snapshot_dirs"""
@@ -16,17 +18,14 @@ def find_read_only_snapshots(snapshot_dirs: list[pathlib.Path]) -> list[pathlib.
         elif os.access(snapshot, os.R_OK):  # snapshot is read
             read_only_snapshots.append(snapshot)
         else:  # This snapshot is not accessible
-            logger.error("Could not access snapshot: " + str(snapshot))
-            logger.error("Aborting. No snapshots were changed.")
-            stc.resume_snapper_timers_and_exit(1)
+            raise PermissionError("Could not access snapshot: " + str(snapshot))
     return read_only_snapshots
 
 
 def make_snapshots_writable(snapshot_dirs: list[pathlib.Path], verbose: bool = False):
-    """Makes all snapshots in snapshot_dirs writable using btrfs property set
-    Returns a list of the snapshots that were made writable"""
+    """Makes all snapshots in snapshot_dirs writable using btrfs property set"""
 
-    print(f"\nMaking {len(snapshot_dirs)} snapshots writable")
+    print(f"\nMaking {len(snapshot_dirs)} snapshots writable\n")
 
     for snapshot in snapshot_dirs:
         if verbose:
@@ -37,14 +36,13 @@ def make_snapshots_writable(snapshot_dirs: list[pathlib.Path], verbose: bool = F
             logger.warning("Could not make snapshot read-write: " + str(snapshot))
             logger.warning("Continuing with next snapshot")
 
-        permission_modified_snapshots.append(snapshot)  # intentionally a global variable
+        permission_modified_snapshots.append(snapshot)
 
 
 def restore_snapshot_permissions(verbose: bool = False):
     """Makes all snapshots in snapshot_dirs read-only using btrfs property set"""
-    global permission_modified_snapshots
 
-    print(f"\nMaking {len(permission_modified_snapshots)} snapshots read-only")
+    print(f"\nMaking {len(permission_modified_snapshots)} snapshots read-only again")
     for snapshot in permission_modified_snapshots:
         if verbose:
             logger.info("Making snapshot read-only again using BTRFS: " + str(snapshot))
